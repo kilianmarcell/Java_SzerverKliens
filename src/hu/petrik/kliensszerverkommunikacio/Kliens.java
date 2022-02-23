47da8012fb48a8e360eb30148c10cfc6f80706ad
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-23 14:59:43 +0100

    Szerver kész

---
+++ src/hu/petrik/kliensszerverkommunikacio/Kliens.java
@@ -0,0 +1,14 @@
+package hu.petrik.kliensszerverkommunikacio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Kliens {
+    public static void main(String[] args) {
+        try {
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/hu/petrik/kliensszerverkommunikacio/Szerver.java
@@ -0,0 +1,29 @@
+package hu.petrik.kliensszerverkommunikacio;
+
+import javax.xml.crypto.Data;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Szerver {
+    public static void main(String[] args) {
+        System.out.println("Szerver indulása");
+
+        try {
+            ServerSocket socket = new ServerSocket(8080);
+            Socket kapcsolat = socket.accept();
+
+            DataInputStream ugyfeltol = new DataInputStream(kapcsolat.getInputStream());
+            DataOutputStream ugyfelnek = new DataOutputStream(kapcsolat.getOutputStream());
+            InetAddress ugyfel = kapcsolat.getInetAddress();
+
+            System.out.println("Az ügyfél neve: " + ugyfel.getHostName());
+            System.out.println("Az ügyfél címe: " + ugyfel.getHostAddress());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
