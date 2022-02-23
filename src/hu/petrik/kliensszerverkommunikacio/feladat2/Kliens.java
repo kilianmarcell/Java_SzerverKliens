93c492e4d84c6057bf3cc9fae334ce35515c8cac
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-23 16:09:28 +0100

    2. feladat kész

--- src/hu/petrik/kliensszerverkommunikacio/feladat2/Kliens.java
+++ src/hu/petrik/kliensszerverkommunikacio/feladat2/Kliens.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Kliens {
     public static void main(String[] args) {
@@ -12,6 +13,39 @@ public class Kliens {
 
             DataInputStream szervertol = new DataInputStream(kapcsolat.getInputStream());
             DataOutputStream szervernek = new DataOutputStream(kapcsolat.getOutputStream());
+
+            Scanner s = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Kérem az a oldalt!: ");
+                int aOldal = s.nextInt();
+
+                System.out.print("Kérem az b oldalt!: ");
+                int bOldal = s.nextInt();
+
+                szervernek.writeInt(aOldal);
+                szervernek.writeInt(bOldal);
+
+                szervernek.flush();
+
+                int menu;
+
+                do {
+                    System.out.println("Melyik menüpontot választja az alábbi lehetőségek közül?:\n" +
+                            "1: Kerület\n" +
+                            "2: Terület\n" +
+                            "3: Négyzet-e\n" +
+                            "4: Átló mérete\n" +
+                            "5: Kilépés\n");
+
+                    menu = s.nextInt();
+
+                    szervernek.writeInt(menu);
+                    szervernek.flush();
+
+                    System.out.println(szervertol.readUTF());
+                } while (menu != 5);
+            }
         } catch (IOException e) {
             System.out.println(e);
         }
--- src/hu/petrik/kliensszerverkommunikacio/feladat2/UgyfelKiszolgalo.java
+++ src/hu/petrik/kliensszerverkommunikacio/feladat2/UgyfelKiszolgalo.java
@@ -19,7 +19,25 @@ public class UgyfelKiszolgalo implements Runnable {
             DataOutputStream ugyfelnek = new DataOutputStream(kapcsolat.getOutputStream());
 
             while (true) {
+                int aOldal = ugyfeltol.readInt();
+                int bOldal = ugyfeltol.readInt();
+                int menu;
 
+                do {
+                    menu = ugyfeltol.readInt();
+                    switch (menu) {
+                        case 1: ugyfelnek.writeUTF("A téglalap kerülete: " + (2 * (aOldal + bOldal)) + "\n\n");
+                            break;
+                        case 2: ugyfelnek.writeUTF("A téglalap területe: " + (aOldal * bOldal) + "\n\n");
+                            break;
+                        case 3: ugyfelnek.writeUTF(aOldal == bOldal ? "Ez a téglalap egy négyzet!\n\n" : "Ez a téglalap nem egy négyzet!\n\n");
+                            break;
+                        case 4: ugyfelnek.writeUTF("A téglalap átló mérete: " + ( Math.sqrt(Math.pow(aOldal, 2) + Math.pow(bOldal, 2))) + "\n\n");
+                            break;
+                        case 5: ugyfelnek.writeUTF("Kilépett!");
+                            break;
+                    }
+                } while (menu != 5);
             }
         } catch (IOException e) {
             System.out.println(e);
